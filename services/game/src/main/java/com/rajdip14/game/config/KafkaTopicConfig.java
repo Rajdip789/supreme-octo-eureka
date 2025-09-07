a447aa9e2a714408ec72d6376d085d9b10e36911
rajdip14 <palrajdip33@gmail.com>
2025-09-07 20:07:19 +0530

    added websocketconfig and kafkatopicconfig

---
+++ services/game/src/main/java/com/rajdip14/game/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.rajdip14.game.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic gameScoreTopic() {
+        return TopicBuilder
+                .name("game-scores")
+                .build();
+    }
+}
